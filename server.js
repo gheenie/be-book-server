f5eb88085eca1648ca47c15cda9003cf269ffacb
infoomics <sbx_94@hotmail.com>
2023-02-06 11:53:19 +0000

    completed challenge 3

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 // 1. Create a new file called `server.js` and using Node's `http` module create a web server that responds with a status 200 and a greeting when it receives a GET request on the path `/api` (e.g. `{ message: "Hello! }`)
 // 2. Add a GET `/api/books` endpoint that responds with a status 200 and a JSON object, that has a key of `books` with a value of the array of books from the `./data/books.json` file.
-
+//Add a GET /api/authors endpoint that responds with a status 200 and a JSON object, that has a key of authors with a value of the array of authors from the ./data/authors.json file.
 
 const http = require('http')
 const fs = require('fs/promises')
@@ -13,6 +13,7 @@ const server = http.createServer((request, response) => {
       response.write(JSON.stringify({ msg: 'Hello!' }));
       response.end();
     }
+
     if (url === '/api/books' && method === 'GET') {
         response.setHeader('Content-Type', 'application/json');
         response.statusCode = 200;
@@ -21,12 +22,22 @@ const server = http.createServer((request, response) => {
             const books = JSON.parse(data)
             const body = JSON.stringify({books})
             response.write(body)
-            console.log(body);
             response.end();
         })
       }
 
-    
+      if (url === '/api/authors' && method === 'GET') {
+        response.setHeader('Content-Type', 'application/json');
+        response.statusCode = 200;
+
+        fs.readFile(`${__dirname}/data/authors.json`, 'utf8')
+        .then((data)=> {
+            const authors = JSON.parse(data)
+            const body = JSON.stringify({authors})
+            response.write(body)
+            response.end();
+        })
+      }
   });
   
   server.listen(9090, (err) => {
