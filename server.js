09cdc9a14f13fcfb0781f67e047a8621d875ae68
infoomics <sbx_94@hotmail.com>
2023-02-06 12:27:13 +0000

    completed challenge 4

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const fs = require('fs/promises')
 
 const server = http.createServer((request, response) => {
     const { method, url } = request;
+
     if (url === '/api' && method === 'GET') {
       response.setHeader('Content-Type', 'application/json');
       response.statusCode = 200;
@@ -15,30 +16,48 @@ const server = http.createServer((request, response) => {
     }
 
     if (url === '/api/books' && method === 'GET') {
-        response.setHeader('Content-Type', 'application/json');
-        response.statusCode = 200;
-        fs.readFile(`${__dirname}/data/books.json`, 'utf8')
-        .then((data)=> {
-            const books = JSON.parse(data)
-            const body = JSON.stringify({books})
-            response.write(body)
-            response.end();
-        })
-      }
+      response.setHeader('Content-Type', 'application/json');
+      response.statusCode = 200;
+      fs.readFile(`${__dirname}/data/books.json`, 'utf8')
+      .then((data)=> {
+          const books = JSON.parse(data)
+          const body = JSON.stringify({books})
+          response.write(body)
+          response.end();
+      })
+    }
 
-      if (url === '/api/authors' && method === 'GET') {
-        response.setHeader('Content-Type', 'application/json');
-        response.statusCode = 200;
+    if (url === '/api/authors' && method === 'GET') {
+      response.setHeader('Content-Type', 'application/json');
+      response.statusCode = 200;
 
-        fs.readFile(`${__dirname}/data/authors.json`, 'utf8')
-        .then((data)=> {
-            const authors = JSON.parse(data)
-            const body = JSON.stringify({authors})
-            response.write(body)
-            response.end();
-        })
-      }
-  });
+      fs.readFile(`${__dirname}/data/authors.json`, 'utf8')
+      .then((data)=> {
+          const authors = JSON.parse(data)
+          const body = JSON.stringify({authors})
+          response.write(body)
+          response.end();
+      })
+    }
+
+    const urlIsBookId = /\/api\/books\//.test(url);
+
+    if (urlIsBookId && method === 'GET') {
+      response.setHeader('Content-Type', 'application/json');
+      response.statusCode = 200;
+
+      fs.readFile(`${__dirname}/data/books.json`, 'utf8')
+      .then((data)=> {
+        const books = JSON.parse(data)
+        const bookId = url.substring(11);
+        // Iterate through books for more accuracy.
+        const book = books[bookId - 1];
+        const body = JSON.stringify({book})
+        response.write(body)
+        response.end();
+      })
+    }
+});
   
   server.listen(9090, (err) => {
     if (err) console.log(err);
